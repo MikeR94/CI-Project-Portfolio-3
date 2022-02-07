1620a255faac4898f9ea94697d0963842b5a09f2
miker94 <michael_ralph@hotmail.com>
2022-02-07 22:06:41 +0000

    Add functionality to load an display a set of questions to the user

--- run.py
+++ run.py
@@ -17,9 +17,67 @@ SHEET = GSPREAD_CLIENT.open("ci_project_portfolio_3")
 sales = SHEET.worksheet("sales")
 data = sales.get_all_values()
 
+class Question:
+    """
+    Questions instance, gets the question
+    and the answer
+    """
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+questions = [
+     "Easy F1 Question 1\n \
+     (A) Easy answer 1\n \
+     (B) Easy answer 2\n \
+     (C) Easy answer 3\n",
+
+     "Easy F1 Question 2\n \
+     (A) Easy answer 1\n \
+     (B) Easy answer 2\n \
+     (C) Easy answer 3\n",
+
+     "Easy F1 Question 3\n \
+     (A) Easy answer 1\n \
+     (B) Easy answer 2\n \
+     (C) Easy answer 3\n",
+
+     "Easy F1 Question 4\n \
+     (A) Easy answer 1\n \
+     (B) Easy answer 2\n \
+     (C) Easy answer 3\n",
+
+     "Easy F1 Question 5\n \
+     (A) Easy answer 1\n \
+     (B) Easy answer 2\n \
+     (C) Easy answer 3\n",
+]
+
+question_list = [
+    Question(questions[0], "A"),
+    Question(questions[1], "A"),
+    Question(questions[2], "A"),
+    Question(questions[3], "A"),
+    Question(questions[4], "A"),
+]
+
+def start_quiz():
+    """ Loads the questions and validates the users answer"""
+    score = 0
+    for question in question_list:
+        user_answer = input(question.question).capitalize()
+        if user_answer == question.answer:
+            score += 1
+            print("Correct answer!")
+        else:
+            print("Incorrect answer")
+    print("Quiz finished")
+
+
 def clear_terminal():
     """ Clears the terminal"""
     os.system('clear')
+
 
 def main_menu():
     """ Displays the main menu to the user that allows them to navigate the application"""
@@ -29,7 +87,7 @@ def main_menu():
         print("Great stuff, starting a new quiz now...")
         time.sleep(2)
         clear_terminal()
-        print("Test 12345")
+        start_quiz()
     if user_input == ("B"):
         print("Exiting the game...")
         time.sleep(2)
