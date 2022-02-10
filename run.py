3479fee418bbade3d4205ae21a737b59f89bf3f6
miker94 <michael_ralph@hotmail.com>
2022-02-10 18:13:19 +0000

    Add another import print statement to split the amount of functions due to pep8 error

--- run.py
+++ run.py
@@ -9,7 +9,8 @@ import random
 from tabulate import tabulate
 import gspread
 from google.oauth2.service_account import Credentials
-from print import cyan_string, green_string, magenta_string, multiple_blank_lines, red_string, white_string, yellow_string, blank_line, solid_line, multiple_blank_lines
+from print import cyan_string, green_string, magenta_string, multiple_blank_lines
+from print import red_string, white_string, yellow_string, blank_line, solid_line
 from questions import easy_question_list
 from questions import medium_question_list
 from questions import hard_question_list
