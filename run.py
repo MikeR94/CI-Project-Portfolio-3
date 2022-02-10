a4fc0793d5c4c0b10958788100a9cbf69c9993a0
miker94 <michael_ralph@hotmail.com>
2022-02-10 16:58:28 +0000

    Change the range to only display 9 items for the leaderboards to give more room

--- run.py
+++ run.py
@@ -178,7 +178,7 @@ def select_difficulty():
 def show_leaderboards():
     """ Shows the leaderboards to the user """
     leaderboard.sort((2, 'des'))
-    print(tabulate(data[0:10], tablefmt='fancy_grid'))
+    print(tabulate(data[0:9], tablefmt='fancy_grid'))
     quick_menu()
 
 
