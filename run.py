bebad8f6b6a1e610cd5e8ffe694a9b2a40fb8901
miker94 <michael_ralph@hotmail.com>
2022-02-10 17:39:19 +0000

    Remove 1 multiple_blank_line() and add 2 single blank_lines() since deployed version needed to move up a few lines

--- run.py
+++ run.py
@@ -236,7 +236,8 @@ def show_game_rules():
 def show_fact():
     """ Displays a random F1 fact to the user """
     multiple_blank_lines()
-    multiple_blank_lines()
+    blank_line()
+    blank_line()
     solid_line(new_line_at_end=True)
     random_fact = get_random_fact()
     print(random_fact.center(80))
