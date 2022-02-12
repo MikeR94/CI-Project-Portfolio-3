d8cc17d6946305f499c55827f56ebc2d133c2ebd
miker94 <michael_ralph@hotmail.com>
2022-02-12 16:01:25 +0000

    Fix remaining PEP 8 validation long lines for entire document

--- run.py
+++ run.py
@@ -9,8 +9,9 @@ import random
 from tabulate import tabulate
 import gspread
 from google.oauth2.service_account import Credentials
-from print import cyan_string, formula_1_text, green_string, magenta_string, multiple_blank_lines
-from print import red_string, white_string, yellow_string, blank_line, solid_line, thank_you
+from print import cyan_string, formula_1_text, green_string
+from print import red_string, white_string, yellow_string, blank_line
+from print import solid_line, thank_you, magenta_string, multiple_blank_lines
 from questions import easy_question_list
 from questions import medium_question_list
 from questions import hard_question_list
@@ -126,7 +127,10 @@ def view_calendar():
 
 
 def view_drivers():
-    ''' Shows the user the current F1 2022 drivers and some statistics about them '''
+    '''
+    Shows the user the current F1 2022 drivers
+    and some statistics about them
+    '''
     clear_terminal()
     print(tabulate(show_drivers, headers='firstrow', tablefmt='simple'))
     blank_line()
@@ -199,7 +203,10 @@ def select_track():
 
 
 def days_to_new_season():
-    ''' Countsdown to the F1 2022 season and displays the remaining days to the user '''
+    '''
+    Countsdown to the F1 2022 season and
+    displays the remaining days to the user
+    '''
     futuredate = datetime.strptime('Mar 18 2022  00:00', '%b %d %Y %H:%M')
     count = int((futuredate-nowdate).total_seconds())
     days = count//86400
@@ -269,7 +276,10 @@ def quick_menu(centered=False, f1_quick_menu=False):
     '''Small navigation menu for the user'''
     if centered:
         while True:
-            cyan_string('A) Return to Quiz Hub B) Return to Main Menu\n'.center(80))
+            cyan_string(
+                        'A) Return to Quiz Hub '
+                        'B) Return to Main Menu\n'.center(80)
+                       )
             user_input = input(''.center(38)).upper()
             blank_line()
             if user_input not in {'A', 'B'}:
@@ -277,12 +287,18 @@ def quick_menu(centered=False, f1_quick_menu=False):
             else:
                 break
         if user_input == ('A'):
-            magenta_string(f'Understood {name}, redirecting back to the Quiz Hub...'.center(80))
+            magenta_string(
+                          f'Understood {name}, redirecting '
+                          f'back to the Quiz Hub...'.center(80)
+                          )
             time.sleep(2)
             clear_terminal()
             quiz_hub()
         if user_input == ('B'):
-            magenta_string(f'Understood {name}, redirecting back to the Main Menu...'.center(80))
+            magenta_string(
+                           f'Understood {name}, redirecting '
+                           f'back to the Main Menu...'.center(80)
+                          )
             time.sleep(2)
             clear_terminal()
             main_menu()
@@ -304,7 +320,10 @@ def quick_menu(centered=False, f1_quick_menu=False):
             clear_terminal()
             f1_info_hub()
         if user_input == ('B'):
-            magenta_string(f'Understood {name}, redirecting back to the Main Menu...')
+            magenta_string(
+                          f'Understood {name}, redirecting '
+                          f'back to the Main Menu...'
+                          )
             time.sleep(2)
             clear_terminal()
             main_menu()
@@ -318,12 +337,18 @@ def quick_menu(centered=False, f1_quick_menu=False):
             else:
                 break
         if user_input == ('A'):
-            magenta_string(f'Understood {name}, redirecting back to the Quiz Hub...')
+            magenta_string(
+                          f'Understood {name}, redirecting '
+                          f'back to the Quiz Hub...'
+                          )
             time.sleep(2)
             clear_terminal()
             quiz_hub()
         if user_input == ('B'):
-            magenta_string(f'Understood {name}, redirecting back to the Main Menu...')
+            magenta_string(
+                          f'Understood {name}, redirecting '
+                          f'back to the Main Menu...'
+                          )
             time.sleep(2)
             clear_terminal()
             main_menu()
@@ -339,7 +364,10 @@ def fact_menu():
         user_input = input(''.center(38)).upper()
         blank_line()
         if user_input not in {'A', 'B', 'C'}:
-            yellow_string('Invalid input! Please enter either A, B, or C \n'.center(80))
+            yellow_string(
+                         'Invalid input! Please enter either '
+                         'A, B, or C \n'.center(80)
+                         )
         else:
             break
     if user_input == ('A'):
@@ -351,7 +379,9 @@ def fact_menu():
         clear_terminal()
         f1_info_hub()
     if user_input == ('B'):
-        magenta_string(f'Understood {name}, redirecting back to the Main Menu...'.center(80))
+        magenta_string(
+                      f'Understood {name}, redirecting '
+                      f'back to the Main Menu...'.center(80))
         time.sleep(2)
         clear_terminal()
         main_menu()
@@ -413,19 +443,28 @@ def show_quiz_stats():
         if item.isdigit():
             total_points.append(int(item))
     solid_line(new_line_at_end=True)
-    white_string(f'Overall points accumulated: {sum(total_points)}\n'.center(80))
+    white_string(
+                f'Overall points accumulated: '
+                f'{sum(total_points)}\n'.center(80)
+                )
     total_correct = []
     for item in leaderboard.col_values(3):
         if item.isdigit():
             total_correct.append(int(item))
     solid_line(new_line_at_end=True)
-    white_string(f'Overall correct questions answered: {sum(total_correct)}\n'.center(80))
+    white_string(
+                f'Overall correct questions answered: '
+                f'{sum(total_correct)}\n'.center(80)
+                )
     total_incorrect = []
     for item in leaderboard.col_values(4):
         if item.isdigit():
             total_incorrect.append(int(item))
     solid_line(new_line_at_end=True)
-    white_string(f'Overall incorrect questions answered: {sum(total_incorrect)}\n'.center(80))
+    white_string(
+                f'Overall incorrect questions answered: '
+                f'{sum(total_incorrect)}\n'.center(80)
+                )
     solid_line(new_line_at_end=True)
     quick_menu(centered=True)
 
@@ -473,7 +512,10 @@ def show_fact():
 
 
 def get_random_fact():
-    ''' Stores all facts in a list, picks one at random and returns it as a string'''
+    '''
+    Stores all facts in a list, picks one
+    at random and returns it as a string
+    '''
     fact_list = []
     for i in fact_data:
         fact_list.append(i)
@@ -484,7 +526,10 @@ def get_random_fact():
 def submit_feedback():
     ''' Allows the user to submit feedback '''
     white_string(f'{name}, please submit your feedback below\n')
-    white_string('If you wish to leave this screen before submitting a message, please click the  run app button below\n')
+    white_string(
+                'If you wish to leave this screen before '
+                'submitting a message, please click the  '
+                'run app button below\n')
     user_feedback = input('Enter feedback: ')
     blank_line()
     magenta_string('Thank you for your feedback, uploading now...\n')
@@ -513,7 +558,9 @@ def quiz_hub():
     blank_line()
     while True:
         if user_input not in {'A', 'B', 'C', 'D', 'E'}:
-            yellow_string('Invalid input! Please enter either A, B, C, D or E\n'.center(80))
+            yellow_string(
+                         'Invalid input! Please enter either '
+                         'A, B, C, D or E\n'.center(80))
             time.sleep(1)
             clear_terminal()
             quiz_hub()
@@ -562,7 +609,10 @@ def f1_info_hub():
     blank_line()
     while True:
         if user_input not in {'A', 'B', 'C', 'D', 'E'}:
-            yellow_string('Invalid input! Please enter either A, B, C, D or E\n'.center(80))
+            yellow_string(
+                         'Invalid input! Please enter either '
+                         'A, B, C, D or E\n'.center(80)
+                         )
             time.sleep(1)
             clear_terminal()
             f1_info_hub()
@@ -597,7 +647,10 @@ def f1_info_hub():
 
 
 def main_menu():
-    ''' Displays the main menu to the user that allows them to navigate the application '''
+    '''
+    Displays the main menu to the user that
+    allows them to navigate the application
+    '''
     multiple_blank_lines()
     red_string(f'Welcome to the Main Menu, {name}\n'.center(80))
     red_string('Please select an option from the menu\n'.center(80))
@@ -610,7 +663,10 @@ def main_menu():
     blank_line()
     while True:
         if user_input not in {'A', 'B', 'C', 'D'}:
-            yellow_string('Invalid input! Please enter either A, B, C, D or E\n'.center(80))
+            yellow_string(
+                         'Invalid input! Please enter either '
+                         'A, B, C, D or E\n'.center(80)
+                         )
             time.sleep(1)
             clear_terminal()
             main_menu()
@@ -651,7 +707,10 @@ while True:
     name = input(''.center(35))
     if len(name) > 7 or name.isspace() or name == '':
         blank_line()
-        yellow_string('Please enter a name that is 7 characters or less'.center(80))
+        yellow_string(
+                     'Please enter a name that is '
+                     '7 characters or less'.center(80)
+                     )
     else:
         break
 time.sleep(2)
